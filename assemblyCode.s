b2421b68b086ec20a4f829e6845c928e44733d96
Frank_enstein <frankfan114@outlook.com>
2023-11-23 20:43:45 +0000

    assemblyCode

--- assemblyCode.s
+++ assemblyCode.s
@@ -1,37 +1,58 @@
-.text                       ; Start of code section
-.equ light_timer, 0xFF     ; Define light_timer constant (address for a timer)
-.equ light_port, 0xFE      ; Define light_port constant (address for light control)
-.equ all_lights, 0b11111   ; Define all_lights constant (binary representation of 5 lights on)
-.equ no_lights, 0x00       ; Define no_lights constant (all lights off)
+.text                           
+.equ light_timer, 0xFF         ; Define light_timer constant (address for a timer)
+.equ light_port, 0xFE          ; Define light_port constant (address for light control)
+.equ button_port, 0xFD         ; Define button_port constant (address for button input)
+.equ all_lights, 0b11111111       ; Define all_lights constant (binary representation of 5 lights on)
+.equ no_lights, 0x0           ; Define no_lights constant (all lights off)
+.equ button_pressed, 0x1      ; Define button_pressed constant (button signal)
+.equ random_timer, 0xfff
 
-main:                       ; Main program start
-    LI a0, 0x01            ; Load the initial light pattern into register a0 (00001)
-    LI t0, 5               ; Load the number of lights (5) into register t0 for countdown
+; a1: dataMem pointer
+; a0: number of light counter
+; a3: random time counter
+
+
+main:                          
+    JAL     ra, wait_for_button         ; Jump to wait_for_button subroutine and link
+    LI      a0, 0x01                    ; Load the initial light pattern into register a0 (00001)
+    LI      t0, 8                       ; Load the number of lights into register t0
+    LI      a3, 0
 
 turn_on_next_light:
-    SB light_port, a0      ; Store the value of a0 to the light port (turn on lights)
-    CALL wait              ; Call the wait function to pause between lights
+    LW      a1, light_port(a0)          ; turn on light accodring to the content in data mem
+    JAL     ra, normal_wait             ; Jump to wait subroutine and link
 
-    SLL a0, a0, 1          ; Shift the lights pattern in a0 left by 1 (turn on the next light)
-    SUBI t0, t0, 1         ; Decrement the light counter
-    BNEZ t0, turn_on_next_light ; If the light counter is not zero, continue the loop
+    ADDI    a0, a0, 1                   ; Shift the lights pattern in a0 left by 1 (turn on the next light)
+    BNE     t0, a0, turn_on_next_light  ; If the light counter is not 8, continue the loop
 
-    CALL wait              ; Call wait to hold all lights on for a moment
-    LI a0, no_lights       ; Load the no_lights pattern into register a0 (turn off all lights)
-    SB light_port, a0      ; Store the value of a0 to the light port (turn off all lights)
-    J main                 ; Jump back to main to restart the sequence
+    JAL     ra, random_wait             ; all lights on  
 
 turn_off_all_lights:
-    LI a0, no_lights       ; Load the no_lights pattern into register a0
-    SB light_port, a0      ; Store the value of a0 to the light port (turn off all lights)
-    CALL wait              ; Call the wait function to pause before the end
+    LI      a0, 0                       ; reset a0 to 0
+    LW      a1, no_lights               ; Store the value of a0 to the light port (turn off all lights             
+    JAL     ra, main
 
-    J main                 ; Jump back to main to restart the sequence
+normal_wait:
+    LBU     a1, light_timer             ; load the normal light counter
+loop_n:
+    ADDI    a1, a1 -1                   ; loop until a1 is 0 
+    BNE     a1, 0, loop_n
+    RET
 
-wait:                       ; Wait function to create delay between light changes
-    LI t1, light_timer     ; Load the address of the timer
-    
-wait_loop:
-    LBU a1, t1             ; Load the current timer value
-    BNEZ a1, wait_loop     ; Loop until the timer value is zero
-    RET                    ; Return from the wait function
+random_wait:
+    BNE     a3, 2**8-1,  random_reset        ; check whether need to reset the random
+    LBU     a1, random_timer(a3)             ; load the random time from DataMem
+loop_r:
+    ADDI    a1, a1, -1                       ; loop until time out
+    BNE     a1, 0, loop_r
+    RET
+
+; wait_for_button:                                    ; Wait function to check for button press
+;     LBU     a1, button_port                         ; Load the value from the button port into register a1
+;     BNE     a1, button_pressed, wait_for_button     ; If button press bit is not equal, keep waiting
+;     RET                      
+
+random_reset:
+    LI  a3, 0                                   ;reset the random timer to 0
+    RET
+
