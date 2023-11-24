0d6ecab819a6f684498d5c92addfb55724dc6691
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2023-11-24 22:21:07 +0000

    delete U-type instruction

--- sign_extend.sv
+++ sign_extend.sv
@@ -1,23 +1,21 @@
 module sign_extend #(
     parameter INSTR_WIDTH = 32
 )(
-    input logic [2:0] ImmSrc,                   
+    input logic [1:0] ImmSrc,                   
     input logic [INSTR_WIDTH-1:0] instr,       
     output logic [INSTR_WIDTH-1:0] ImmExt         
 );
 
     always_comb begin
         case (ImmSrc)
-            3'b000: // I-type
+            2'b00: // I-type
                 ImmExt = {{20{instr[31]}}, instr[31:20]};
-            3'b001: // S-type
+            2'b01: // S-type
                 ImmExt = {{20{instr[31]}}, instr[31:25], instr[11:7]};
-            3'b010: // B-type
+            2'b10: // B-type
                 ImmExt = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
-            3'b011: // J-type
+            2'b11: // J-type
                 ImmExt = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
-            3'b100: // U-type
-                ImmExt = {instr[31:12], {12{1'b0}}};
             default: 
                 ImmExt = {{20{instr[31]}}, instr[31:20]};
         endcase
