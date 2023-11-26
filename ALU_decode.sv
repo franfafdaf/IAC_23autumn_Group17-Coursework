6f7fc3cd01b725573afc162eb4f60e4bb873b29e
lgxi24 <lambert.lgx@gmail.com>
2023-11-26 21:30:58 +0000

    modified ALU_decode.sv

--- ALU_decode.sv
+++ ALU_decode.sv
@@ -1,9 +1,10 @@
 //This module is managed by Guanxi Lu
 module ALU_decode(
     input logic op5, 
-    input logic func3, 
+    input logic [2:0] func3, 
     input logic func75, 
-    input logic ALUOp
+    input logic [1:0] ALUOp
+    output logic [2:0] ALUControl
 );
 
 logic [6:0] decode; 
@@ -11,10 +12,9 @@ assign decode = {ALUOp, func3, op5, func75};
 
 always_comb
 case (decode)
-    00?????: ALUControl = 3'b000; //add
-    01?????: ALUControl = 3'b001; //sub
-    1000000: ALUControl = 3'b000; //add
-    1000001: ALUControl = 3'b000; //add
+    00?????: ALUControl = 3'b000; //add for lw, sw
+    01?????: ALUControl = 3'b001; //sub for beq
+    100000?: ALUControl = 3'b000; //add
     1000010: ALUControl = 3'b000; //add
     1000011: ALUControl = 3'b001; //sub
     10010??: ALUControl = 3'b101; //slt
--- extend.sv
+++ extend.sv
@@ -10,6 +10,7 @@ case (ImmSrc)
     2'b00: ImmExt = {{20{Instr[31]}}, Instr[31:20]}; //for I-type, 12-bit signed immediate
     2'b01: ImmExt = {{20{Instr[31]}}, Instr[31:25], Instr[11:7]}; //for S-type. 12-bit signed immediate
     2'b10: ImmExt = {{20{Instr[31]}}, Instr[7], Instr[30:25], Instr[11:8], 1'b0}; //for B-type, 13-bit signed immediate
+    2'b11: ImmExt = {{12{Instr[31]}}, Instr[19:12], Instr[20], Instr[30:21], 1'b0}; //for J-type, 21-bit signed immediate
     default: ImmExt = 0;
 
 endcase
