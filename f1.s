2925628a36afa6ec21e1a0aabbcaf102c5f49aef
TaylorZYY <147536074+TaylorZYY@users.noreply.github.com>
2023-11-26 10:55:21 +0000

    F1assembly

--- f1.s
+++ f1.s
@@ -1,43 +1,38 @@
 init:
     addi s1, zero, 0x1
-    addi s2, zero, 0xff
-    addi s3, zero, 0x3
-    addi a3, zero, 0x1
+    addi s2, zero, 0xff 
+    addi s3, zero, 0x10
+    addi a3, zero, 0x1 
 
 rst:
     addi a0, zero, 0x0
-    addi a4, zero, 0x0
     addi t0, zero, 0x0
-    jal  zero, mainloop
+    addi t1, zero, 0x0
 
-mainloop:
-    beq  t0, s1, fsm
-    srli a2, a3, 0x3    
-    andi a2, a2, 0x1    
-    xor  a2, a2, a3     
-    andi a2, a2, 0x1    
-    slli a3, a3, 0x1    
-    add  a3, a3, a2     
-    andi a3, a3, 0xf    
+loop:
+    beq t0, s1, fsm
+    srli a2, a3, 0x3
+    andi a2, a2, 0x1
+    xor  a2, a2, a3
+    andi a2, a2, 0x1 
+    slli a3, a3, 0x1 
+    add  a3, a3, a2 
+    andi a3, a3, 0xf
     jal  ra, mainloop
 
 fsm:
-    jal  ra, lightdelay
-    slli t1, a0, 0x1
-    addi a0, t1, 0x1
-    bne  a0, s2, fsm
-    jal  zero, increment
+    addi a0, a0, a0
+    addi a0, a0, 0x1
+    jal ra, lightdelay
+    bne a0, s2, fsm
 
-increment:
-    beq  a4, a3, rst
-    jal  ra, lightdelay
-    addi a4, a4, 0x1
+inrement:
+    beq a3, t1, rst
+    addi t1, t1, 0x1
     jal  ra, increment
 
 lightdelay:
-    addi  a1, zero, 0x0
-    addi  t2, zero, s3
-delay_loop:
     addi  a1, a1, 0x1
-    bne   a1, t2, delay_loop
-    RET      
+    bne   a1, s3, lightdelay
+    addi  a1, zero, 0x0
+    RET
