87162fdcfc9f1e6678a7482adb9b50e21ff08ebc
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2023-11-26 20:01:46 +0000

    data memory in bytes

--- DataMemory.sv
+++ DataMemory.sv
@@ -1,30 +1,32 @@
 module DataMemory #(
-    parameter ADDRESS_WIDTH = 17,
-    parameter DATA_WIDTH = 32
+    parameter ADDRESS_WIDTH = 32, 
+    parameter DATA_WIDTH = 8      
 )(
-    input logic clk,             // Clock signal
-    input logic WE,              // Write enable signal
-    input logic [DATA_WIDTH-1:0] A, // Memory address input
-    input logic [DATA_WIDTH-1:0] WD, // Data input for write operation
-    output logic [DATA_WIDTH-1:0] RD  // Data output for read operation
+    input logic clk,                       // Clock signal
+    input logic WE,                        // Write enable signal
+    input logic [ADDRESS_WIDTH-1:0] A,     // Memory address input
+    input logic [DATA_WIDTH*4-1:0] WD,     // Data input for write operation (32 bits)
+    output logic [DATA_WIDTH*4-1:0] RD     // Data output for read operation (32 bits)
 );
 
-    // Memory array declaration
-    logic [DATA_WIDTH-1:0] mem_array [2**ADDRESS_WIDTH-1:0];
-    
+    logic [DATA_WIDTH-1:0] mem_array [32'h0001FFFF:32'h00000000];
+
     initial begin
-        $readmemh("gaussian.mem", mem_array); 
+        $readmemh("gaussian.mem", mem_array, 'h10000); 
     end
 
     always_comb begin
-        RD = mem_array[A]; // Reading data from memory at address A
+        RD = {mem_array[A+3], mem_array[A+2], mem_array[A+1], mem_array[A]};
     end
 
     always_ff @(posedge clk) begin
         if (WE) begin
-            mem_array[A] <= WD; // Writing data into memory at address A
+            mem_array[A] <= WD[7:0];
+            mem_array[A+1] <= WD[15:8];
+            mem_array[A+2] <= WD[23:16];
+            mem_array[A+3] <= WD[31:24];
         end
     end
-
-
+    
 endmodule
+
