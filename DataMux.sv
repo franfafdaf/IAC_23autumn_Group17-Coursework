9ca6cd7d5cff3221409601c9f1e33f1879a9a645
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2023-11-26 22:24:53 +0000

    add u-type instruction

--- DataMux.sv
+++ DataMux.sv
@@ -1,18 +1,20 @@
 module DataMUX (
-    input logic [1:0]   ResultSrc,    // Selector bit
-    input logic [31:0]  ALUResult,    // ALU result
-    input logic [31:0]  RD,           // Data memory read data
-    input logic [31:0]  PCPlus        // PC + 4
-    output logic [31:0] Result        // Output result
+    input logic [1:0]   ResultSrc, // 2-bit selector
+    input logic [31:0]  ALUResult, // ALU result
+    input logic [31:0]  RD,        // Data memory read data
+    input logic [31:0]  PCPlus,    // PC + 4
+    input logic [31:0]  UImm,      // Result from U-type instruction
+    output logic [31:0] Result    // Output result
 );
 
     always_comb begin
-        case(ResultSrc)
-            2'b00: Result = ALUResult;
-            2'b01: Result = RD;
-            2'b10: Result = PCPlus;
-            default: Result = 32'b0;
+        case (ResultSrc)
+            2'b00: Result = ALUResult; 
+            2'b01: Result = RD;        
+            2'b10: Result = PCPlus;    
+            2'b11: Result = UImm;      
+            default: Result = 32'b0;   
         endcase
     end
-
 endmodule
+
