be31b186e5fc81a2e4c31caed2ef84b756be0238
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2023-11-26 19:29:16 +0000

    DataMux

--- DataMux.sv
+++ DataMux.sv
@@ -1,8 +1,8 @@
 module DataMUX (
-    input logic ResultSrc,             // Selector bit
-    input logic [31:0] ALUResult, // ALU result
-    input logic [31:0] RD,  // Data memory read data
-    output logic [31:0] Result    // Output result
+    input logic         ResultSrc,    // Selector bit
+    input logic [31:0]  ALUResult,    // ALU result
+    input logic [31:0]  RD,           // Data memory read data
+    output logic [31:0] Result        // Output result
 );
 
     assign Result = ResultSrc ? RD : ALUResult;
