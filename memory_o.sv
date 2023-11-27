ca2835adea77d96a1baf88f98d5ce0af00647f2c
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2023-11-27 19:55:04 +0000

    corrected data loaded

---
+++ memory_o.sv
@@ -0,0 +1,19 @@
+module memory_o #(
+    parameter DATA_WIDTH = 32       
+)(
+    input  logic                         LdSrc,    //0: word, 1: byte
+    input  logic    [DATA_WIDTH-1:0]     RD_i,     //Data memory read data
+    input  logic    [DATA_WIDTH-1:0]     A,        //Memory address input
+    output logic    [DATA_WIDTH-1:0]     RD_o      //Corrected Data output 
+);
+
+always_comb begin
+    if (LdSrc) begin                //LBU
+        RD_o = {{24'b0}, rd_i[7:0]};
+    end
+    else begin                      //LW
+        RD_o = RD_i;
+    end
+end
+
+endmodule
