11ab7f0987d25adfedadf45c5db0c954eadec6d2
lgxi24 <lambert.lgx@gmail.com>
2023-11-27 20:01:32 +0000

    merge

---
+++ memory.sv
@@ -0,0 +1,29 @@
+module memory #(
+    parameter ADDRESS_WIDTH = 32, 
+    parameter DATA_WIDTH = 8
+) (
+    input  logic                        clk,
+    input  logic                        WE,
+    input  logic [ADDRESS_WIDTH-1:0]    A,
+    input  logic [ADDRESS_WIDTH-1:0]    WD,
+    output logic [ADDRESS_WIDTH-1:0]    RD
+);
+
+    logic [DATA_WIDTH-1:0] mem_array [32'h0001FFFF:32'h00000000]; 
+
+    initial begin 
+        $readmemh("gaussian.mem", mem_array, 32'h10000);
+    end;
+
+    assign RD = {mem_array[A+3], mem_array[A+2], mem_array[A+1], mem_array[A]};
+        
+    always_ff @(posedge clk) begin
+        if (WE) begin
+            mem_array[A] <= WD[7:0];
+            mem_array[A+1] <= WD[15:8];
+            mem_array[A+2] <= WD[23:16];
+            mem_array[A+3] <= WD[31:24];
+        end
+    end 
+
+endmodule
---
+++ memory_i.sv
@@ -0,0 +1,22 @@
+module memory_i #(
+    parameter DATA_WIDTH = 32
+
+)(
+    input  logic                    StSrc,    //0: word, 1: byte
+    input  logic                    WD_i,     //Data memory write data
+    input  logic[DATA_WIDTH-1:0]    RD,       //Data memory read data
+    input  logic[DATA_WIDTH-1:0]    A,        //Memory address input
+    output logic[DATA_WIDTH-1:0]    WD_o      //Corrected Write Data
+
+);
+
+always_comb begin
+    if (StSrc) begin                 //SB
+        WD_o ={RD[31:8], WD_i[7:0]};
+    end
+    else begin                      //SW
+        WD_o = WD_i;
+    end
+end
+
+endmodule
