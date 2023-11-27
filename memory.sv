bd93881ac8e7670a6dcb8fdedc988da561d490c1
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2023-11-27 21:20:12 +0000

    Update memory.sv

--- memory.sv
+++ memory.sv
@@ -1,15 +1,15 @@
 module memory #(
-    parameter ADDRESS_WIDTH = 32, 
-    parameter DATA_WIDTH = 8
+    parameter DATA_WIDTH = 32, 
+    parameter BYTE_WIDTH = 8
 ) (
-    input  logic                        clk,
-    input  logic                        WE,
-    input  logic [ADDRESS_WIDTH-1:0]    A,
-    input  logic [ADDRESS_WIDTH-1:0]    WD,
-    output logic [ADDRESS_WIDTH-1:0]    RD
+    input  logic                     clk,
+    input  logic                     WE,
+    input  logic [DATA_WIDTH-1:0]    A,
+    input  logic [DATA_WIDTH-1:0]    WD,
+    output logic [DATA_WIDTH-1:0]    RD
 );
 
-    logic [DATA_WIDTH-1:0] mem_array [32'h0001FFFF:32'h00000000]; 
+    logic [BYTE_WIDTH-1:0] mem_array [32'h0001FFFF:32'h00000000]; 
 
     initial begin 
         $readmemh("gaussian.mem", mem_array, 32'h10000);
