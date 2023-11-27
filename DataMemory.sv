1f2c18c2b409b683df5746d7752d8308ff33bfcf
lgxi24 <lambert.lgx@gmail.com>
2023-11-27 20:05:24 +0000

    modified conflict

--- DataMemory.sv
+++ DataMemory.sv
@@ -38,27 +38,3 @@ module DataMemory#(
     );
 
 endmodule
-
-=======
-module DataMemory #(
-    parameter DataLength = 32,
-               
-) (
-    input logic                     clk;
-    input logic                     WE;//write enable
-    input logic [DataLength-1: 0]   A;// aluAddress 
-    input logic [Datalength-1: 0]   WD;//write data
-    output logic [DataLength-1: 0]  RD// read data
-);
-logic [DataLength-1:0] DataArray [0x0001FFFF: 0X00000000];
-
-initial
-    $readmemh("data.mem", DataArray);
-
-always_ff @(posedge clk) begin
-    if (WE)
-        mem[A] <= WD;
-    RD <= mem[A]; //in same cycle?
-end
-endmodule
->>>>>>> origin/fhc
