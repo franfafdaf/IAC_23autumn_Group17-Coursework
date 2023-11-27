926a378f62703cd1fecefe140646c3d4fe2c0111
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2023-11-27 20:08:15 +0000

    prepare data to be loaded

--- memory_o.sv
+++ memory_o.sv
@@ -9,7 +9,7 @@ module memory_o #(
 
 always_comb begin
     if (LdSrc) begin                //LBU
-        RD_o = {{24'b0}, rd_i[7:0]};
+        RD_o = {{24'b0}, RD_i[7:0]};
     end
     else begin                      //LW
         RD_o = RD_i;
