3d4acecc2d17419e4923c46556ddf838c96bdcfe
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2023-11-27 19:54:09 +0000

    Update DataMemory.sv

--- DataMemory.sv
+++ DataMemory.sv
@@ -1,43 +1,40 @@
-module DataMemory #(
-    parameter ADDRESS_WIDTH = 32, 
-    parameter DATA_WIDTH = 8      
+module DataMemory#(
+    parameter DATA_WIDTH = 32
 )(
-    input logic clk,                       // Clock signal
-    input logic WE,                        // Write enable signal
-    input logic BE,                        // Byte enable signal
-    input logic [ADDRESS_WIDTH-1:0] A,     // Memory address input
-    input logic [DATA_WIDTH*4-1:0] WD,     // Data input for write operation (32 bits)
-    output logic [DATA_WIDTH*4-1:0] RD     // Data output for read operation (32 bits)
+    input  logic                clk,          // Clock input
+    input  logic                WE,          // Write Enable signal
+    input  logic                StSrc,       // Store Type signal
+    input  logic                LdSrc,       // Load Type signal
+    input  logic [DATA_WIDTH:0] A,           // Address input
+    input  logic [DATA_WIDTH:0] WD,          // Write Data input
+    output logic [DATA_WIDTH:0] RD           // Read Data output
 );
 
-    logic [DATA_WIDTH-1:0] mem_array [32'h0001FFFF:32'h00000000];
+    logic [31:0] corrected_WD;
+    logic [31:0] read_data_internal;
 
-    initial begin
-        $readmemh("gaussian.mem", mem_array, 'h10000); 
-    end
+    memory_i my_mem_i (
+        .StSrc(StSrc),
+        .WD_i(WD),
+        .RD(read_data_internal),
+        .A(A),
+        .WD_o(corrected_WD)
+    );
 
-    always_ff @(posedge clk) begin
-        if (WE) begin
-            if (BE) begin                //SB
-                mem_array[A] <= WD[7:0];
-            end
-            else begin                   //SW
-                mem_array[A] <= WD[7:0];
-                mem_array[A+1] <= WD[15:8];
-                mem_array[A+2] <= WD[23:16];
-                mem_array[A+3] <= WD[31:24];
-            end
-        end
-    end
+    memory my_mem (
+        .clk(clk),
+        .WE(WE),
+        .A(A),
+        .WD(corrected_WD),
+        .RD(read_data_internal)
+    );
 
-    always_comb begin
-        if (BE) begin                        //LBU
-            RD = {24'b0, mem_array[Address]};
-        end
-        else begin                           //LW
-            RD = {mem_array[A+3], mem_array[A+2], mem_array[A+1], mem_array[A]};
-        end
-    end
+    memory_o my_mem_o (
+        .LdSrc(LdSrc),
+        .RD_i(read_data_internal),
+        .A(A),
+        .RD_o(RD)
+    );
 
 endmodule
 
