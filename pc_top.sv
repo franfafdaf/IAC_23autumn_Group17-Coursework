3ea5ac88d891ea48c781437d2a00f1734cc2a544
Frank_enstein <frankfan114@outlook.com>
2023-11-28 11:38:47 +0000

    PC modify

--- pc_top.sv
+++ pc_top.sv
@@ -1,10 +1,10 @@
 module pc_top#(
     parameter PC_WIDTH = 32
 )(
-    input logic clk,
-    input logic rst,
-    input logic PCSrc,
-    input logic [PC_WIDTH-1:0] ImmExt,
+    input logic                 clk,
+    input logic                 rst,
+    input logic                 PCSrc,
+    input logic [PC_WIDTH-1:0]  ImmExt,
     output logic [PC_WIDTH-1:0] PC
 );
 
@@ -12,29 +12,14 @@ module pc_top#(
     logic [PC_WIDTH-1:0] PC_Target;
     logic [PC_WIDTH-1:0] PCNext;
 
+
+    assign  NextPC = PC + 4;
+    assign  PC_Target = PC + ImmExt;
+    assign  PC_Next = PCSrc ? PC_Target : PC_Plus;
+
     always_ff @(posedge clk or posedge rst)begin
         if (rst) PC <= 0;
         else PC <= PCNext;
     end
 
-    pc_mux my_pc_mux(
-        .PCSrc(PCSrc),
-        .PC_Plus(PC_Plus),
-        .PC_Target(PC_Target),
-        .PC_Next(PCNext)
-    );
-
-    pc_plus my_pc_plus(
-        .PC(PC),
-        .NextPC(PC_Plus)
-    );
-
-    pc_target my_pc_target(
-        .PC(PC),
-        .ImmExt(ImmExt),
-        .PC_target(PC_Target)
-    );
-
 endmodule
-
-
