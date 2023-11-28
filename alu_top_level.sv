529f67693a8ccd2bb43c26deecb8fb17b132fcf5
TaylorZYY <147536074+TaylorZYY@users.noreply.github.com>
2023-11-28 21:35:57 +0000

    add WE3, separate alu and regfile

--- alu_top_level.sv
+++ alu_top_level.sv
@@ -1,34 +1,18 @@
 module alu_top_level(
-    input logic clk,
-    input logic [4:0] A1,
-    input logic [4:0] A2,
-    input logic [4:0] A3,
-    input logic [31:0] WD3,
     input logic [31:0] ImmExt,
     input logic ALUsrcB,
     input logic [2:0] ALUControl,
     input logic [31:0] PC,
     input logic ALUSrcA,
     output logic [31:0] ALUResult, 
-    output logic [31:0] RD2, 
+    input logic [31:0] RD2, 
+    input logic [31:0] RD1,
     output logic Zero,
     output logic [31:0] a0
 );
 
-    logic [31:0] RD1;
     logic [31:0] SrcA;
     logic [31:0] SrcB;
-
-    reg_file myreg_file(
-        .clk(clk),
-        .A1(A1),
-        .A2(A2),
-        .A3(A3),
-        .WD3(WD3),
-        .RD1(RD1),
-        .RD2(RD2),
-        .a0(a0)
-    );
 
     alu_muxA mymuxA(
         .RD1(RD1),
--- reg_file.sv
+++ reg_file.sv
@@ -4,6 +4,7 @@ module reg_file(
     input logic [4:0] A2,
     input logic [4:0] A3,
     input logic [31:0] WD3,
+    input logic WE3,
     output logic [31:0] RD1,
     output logic [31:0] RD2,
     output logic [31:0] a0
@@ -15,8 +16,10 @@ assign RD1 = mem[A1];
 assign RD2 = mem[A2];
 
 always_ff @(posedge clk) begin
-    mem[AD3] <= WD3;
-    a0 <= WD3;
+    if (WE3) begin
+        mem[AD3] <= WD3;
+        a0 <= WD3;
+    end
 end
 
 endmodule
