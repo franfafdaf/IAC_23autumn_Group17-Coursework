8436dd6e27587e04a6164b6032a294b0e34888b2
Frank_enstein <frankfan114@outlook.com>
2023-11-28 11:20:22 +0000

    add WE3 for regFile

--- reg_file.sv
+++ reg_file.sv
@@ -4,6 +4,7 @@ module reg_file(
     input logic [4:0]       A2,
     input logic [4:0]       A3,
     input logic [31:0]      WD3,
+    input logic             WE3,
     output logic [31:0]     RD1,
     output logic [31:0]     RD2,
     output logic [31:0]     a0
@@ -15,8 +16,9 @@ assign RD1 = mem[A1];
 assign RD2 = mem[A2];
 
 always_ff @(posedge clk) begin
-    mem[A3] <= WD3;
-    a0 <= WD3;
+    if (WE3 == 1'b1)
+        mem[A3] <= WD3;
+        a0 <= WD3;
 end
 
 endmodule
