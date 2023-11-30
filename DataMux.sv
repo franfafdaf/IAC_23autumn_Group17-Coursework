c4a6135593346b9a64454955b24c7be1a8a59d7b
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2023-11-30 20:01:06 +0000

    updated

--- DataMux.sv
+++ DataMux.sv
@@ -1,9 +1,9 @@
-module DataMUX (
+// <<<<<<< HEAD
+module DataMux (
     input logic [1:0]   ResultSrc, // 2-bit selector
     input logic [31:0]  ALUResult, // ALU result
     input logic [31:0]  RD,        // Data memory read data
     input logic [31:0]  PCPlus,    // PC + 4
-    input logic [31:0]  UImm,      // Result from U-type instruction
     output logic [31:0] Result    // Output result
 );
 
@@ -11,10 +11,8 @@ module DataMUX (
         case (ResultSrc)
             2'b00: Result = ALUResult; 
             2'b01: Result = RD;        
-            2'b10: Result = PCPlus;    
-            2'b11: Result = UImm;      
+            2'b10: Result = PCPlus;     
             default: Result = 32'b0;   
         endcase
     end
 endmodule
-
