715015b5cad5e7bbc58dddd9a8f0a6462aee4fdb
TaylorZYY <147536074+TaylorZYY@users.noreply.github.com>
2023-11-30 10:22:46 +0000

    add shift

--- reg_file.sv
+++ reg_file.sv
@@ -1,25 +1,27 @@
 module reg_file(
-    input logic clk,
-    input logic [4:0] A1,
-    input logic [4:0] A2,
-    input logic [4:0] A3,
-    input logic [31:0] WD3,
-    input logic WE3,
-    output logic [31:0] RD1,
-    output logic [31:0] RD2,
-    output logic [31:0] a0
+    input logic             clk,
+    input logic [4:0]       A1,
+    input logic [4:0]       A2,
+    input logic [4:0]       A3,
+    input logic [31:0]      WD3,
+    input logic             WE3,
+
+    output logic [31:0]     RD1,
+    output logic [31:0]     RD2,
+    output logic [31:0]     a0
 );
 
 logic [31:0] mem [31:0];
 
 assign RD1 = mem[A1];
 assign RD2 = mem[A2];
+assign a0= mem[10];
 
 always_ff @(posedge clk) begin
-    if (WE3) begin
-        mem[AD3] <= WD3;
-        a0 <= WD3;
-    end
+    assign mem[0] = 0;
+    if (WE3 == 1'b1)
+        mem[A3] <= WD3;
 end
+
 
 endmodule
