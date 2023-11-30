21bfbbd5e97630e4cb43fa2087b94cfeffa4cd37
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2023-11-30 20:42:00 +0000

    add shift operation

--- f1.s
+++ f1.s
@@ -1,40 +1,40 @@
-//not sure for shift operation
 init:
-    addi s1, zero, 0x1    /* initial state */
-    addi s2, zero, 0xff   /* final state */
-    addi s3, zero, 0x18   /* determines delay between lights */
-    addi a3, zero, 0x1    /* LFSR */
+    addi s1, zero, 0x1      
+    addi s2, zero, 0xff     // 0xff represents all lights on
+    addi s3, zero, 0x3      // Fixed delay
+    addi a3, zero, 0x1      // Seed for LFSR
 
 rst:
-    addi a0, zero, 0x0    /* reset all */
-    addi a4, zero, 0x0 
-    addi t0, zero, 0x0 
+    addi a0, zero, 0x0      // Turn off all lights
+    addi a4, zero, 0x0      // Reset random delay counter
+    addi t0, zero, 0x0      // Reset temp register
 
 mainloop:
-    beq  t0, s1, fsm      /* branch to fsm if in initial state */ 
-    //srli a2, a3, 0x3      /* LFSR */
+    beq  t0, s1, fsm       // Check if it's time to start light sequence
+    srli a2, a3, 0x3       //LFSR
     andi a2, a2, 0x1
     xor  a2, a2, a3
     andi a2, a2, 0x1 
-    add  a3, a3, a3 
+    slli a3, a3, 0x1 
     add  a3, a3, a2 
     andi a3, a3, 0xf
-    jal  ra, mainloop     /* Loop  */
+    jal  ra, mainloop      
 
 fsm:
-    jal  ra, lightdelay   /* add const delay */
-    add  t1, a0, a0       /* shift left by 1 */
-    addi a0, t1, 0x1      /* add 1 to change to the next state */
-    bne  a0, s2, fsm      /* loop only if not all lights are on */
+    jal  ra, lightdelay    
+    slli t1, a0, 0x1       // Shift left temp bits
+    addi a0, t1, 0x1       // Add 1 to shifted temp bits (turn on next light)
+    bne  a0, s2, fsm       // Continue if not all lights are on
+    jal  ra, randomdelay   // Jump to random delay after all lights are on
 
-increment:
-    beq  a4, a3, rst
-    jal  ra, lightdelay   /* jump to counter */
-    addi a4, a4, 0x1      /* inc delay */
-    jal  ra, increment    /* run until delay finished */
+randomdelay:
+    beq  a4, a3, rst       // If random delay is met, reset (turn off lights)
+    addi a4, a4, 0x1       // Increment delay counter
+    jal  ra, randomdelay   // Continue random delay
 
 lightdelay:
-    addi  a1, a1, 0x1     /* inc count */
-    bne   a1, s3, lightdelay
-    addi  a1, zero, 0x0   /* reset count */
+    addi  a1, a1, 0x1      
+    bne   a1, s3, lightdelay // Continue delay if not met
+    addi  a1, zero, 0x0    // Reset counter
     RET
+
