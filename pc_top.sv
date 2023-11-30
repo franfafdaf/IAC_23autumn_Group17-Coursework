6aa6b993b8752f16c7e7337d3006bb28a0c2efe2
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2023-11-30 20:01:29 +0000

    Add files via upload

--- pc_top.sv
+++ pc_top.sv
@@ -1,40 +1,29 @@
 module pc_top#(
     parameter PC_WIDTH = 32
 )(
-    input logic clk,
-    input logic rst,
-    input logic PCSrc,
-    input logic [PC_WIDTH-1:0] ImmExt,
+    input logic                 clk,
+    input logic                 rst,
+    input logic                 PCSrc,
+    input logic                 JalSrc,
+    input logic [PC_WIDTH-1:0]  ImmExt,
+    input logic [PC_WIDTH-1:0]  RD1,
+    output logic [PC_WIDTH-1:0] PC_Plus,
     output logic [PC_WIDTH-1:0] PC
 );
 
-    logic [PC_WIDTH-1:0] PC_Plus;
     logic [PC_WIDTH-1:0] PC_Target;
-    logic [PC_WIDTH-1:0] PCNext;
+    logic [PC_WIDTH-1:0] PC_Jump;
+    logic [PC_WIDTH-1:0] PC_Next;
+
+
+    assign  PC_Plus = PC + 4;
+    assign  PC_Jump = PC + ImmExt;
+    assign  PC_Target = JalSrc ? PC_Jump : RD1; 
+    assign  PC_Next = PCSrc ? PC_Target : PC_Plus;
 
     always_ff @(posedge clk or posedge rst)begin
         if (rst) PC <= 0;
-        else PC <= PCNext;
+        else PC <= PC_Next;
     end
 
-    pc_mux my_pc_mux(
-        .PCSrc(PCSrc),
-        .PC_Plus(PC_Plus),
-        .PC_Target(PC_Target),
-        .PC_Next(PCNext)
-    );
-
-    pc_plus my_pc_plus(
-        .PC(PC),
-        .NextPC(PC_Plus)
-    );
-
-    pc_target my_pc_target(
-        .PC(PC),
-        .ImmExt(ImmExt),
-        .PC_target(PC_Target)
-    );
-
 endmodule
-
-
