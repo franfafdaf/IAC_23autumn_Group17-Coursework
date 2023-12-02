7fd365f813d43f76aee15758f1f83589b4102414
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2023-12-02 15:38:36 +0000

    modify for pipelining

--- DataMux.sv
+++ DataMux.sv
@@ -1,18 +1,19 @@
-// <<<<<<< HEAD
 module DataMux (
-    input logic [1:0]   ResultSrc, // 2-bit selector
-    input logic [31:0]  ALUResult, // ALU result
-    input logic [31:0]  RD,        // Data memory read data
-    input logic [31:0]  PCPlus,    // PC + 4
-    output logic [31:0] Result    // Output result
+    input logic [1:0]   ResultSrcW, // 2-bit selector
+    input logic [31:0]  ALUResultW, // ALU result
+    input logic [31:0]  ReadDataW,        // Data memory read data
+    input logic [31:0]  PCPlusW,    // PC + 4
+    output logic [31:0] ResultW    // Output result
 );
 
     always_comb begin
-        case (ResultSrc)
-            2'b00: Result = ALUResult; 
-            2'b01: Result = RD;        
-            2'b10: Result = PCPlus;     
+        case (ResultSrcW)
+            2'b00: ResultW = ALUResultW; 
+            2'b01: ResultW = ReadDataW;        
+            2'b10: ResultW = PCPlusW;     
             default: Result = 32'b0;   
         endcase
     end
+
 endmodule
+
