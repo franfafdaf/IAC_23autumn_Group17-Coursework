71e73c19c3c38498cdf169fd8c70b50fe15e3d6b
Frank_enstein <frankfan114@outlook.com>
2023-12-02 18:42:11 +0000

    control unit&aludecode modify

--- ALU_decode.sv
+++ ALU_decode.sv
@@ -9,7 +9,7 @@ module ALU_decode(
     input logic [2:0]   funct3, 
     input logic         func75, 
     input logic [1:0]   ALUOp,
-    output logic [2:0]  ALUControl
+    output logic [2:0]  ALUControlD
 );
 
 logic [1:0] decode;
@@ -19,29 +19,29 @@ assign decode = {op5, func75};
 
 always_comb begin
     if (ALUOp == 2'b00) begin 
-        if (funct3 == 3'b001) ALUControl = 3'b111; //shift to left
-        else if (funct3 == 3'b101) ALUControl = 3'b100; //shift to right
-        else if (funct3 == 3'b111) ALUControl = 3'b010; //and
-        else ALUControl = 3'b000; //add
+        if (funct3 == 3'b001) ALUControlD = 3'b111; //shift to left
+        else if (funct3 == 3'b101) ALUControlD = 3'b100; //shift to right
+        else if (funct3 == 3'b111) ALUControlD = 3'b010; //and
+        else ALUControlD = 3'b000; //add
     end 
-    else if (ALUOp == 2'b01) ALUControl = 3'b001; //sub
+    else if (ALUOp == 2'b01) ALUControlD = 3'b001; //sub
     else if (ALUOp == 2'b10) begin
         if (funct3 == 3'b000) begin
-            if (decode == 2'b00) ALUControl = 3'b000; //add
-            else if (decode == 2'b01) ALUControl = 3'b000; //add
-            else if (decode == 2'b10) ALUControl = 3'b000; //add
-            else  ALUControl = 3'b001; //sub， decode == 2'b11
+            if (decode == 2'b00) ALUControlD = 3'b000; //add
+            else if (decode == 2'b01) ALUControlD = 3'b000; //add
+            else if (decode == 2'b10) ALUControlD = 3'b000; //add
+            else  ALUControlD = 3'b001; //sub， decode == 2'b11
         end
-        else if (funct3 == 3'b100) ALUControl = 3'b101; //slt
-        else if (funct3 == 3'b110) ALUControl = 3'b011; //or
-        else if (funct3 == 3'b111) ALUControl = 3'b010; //and
-        else ALUControl = 3'b000;
+        else if (funct3 == 3'b100) ALUControlD = 3'b101; //slt
+        else if (funct3 == 3'b110) ALUControlD = 3'b011; //or
+        else if (funct3 == 3'b111) ALUControlD = 3'b010; //and
+        else ALUControlD = 3'b000;
     end
     else if (ALUOp == 2'b11) begin
-        if (op5 == 0) ALUControl = 3'b000; // add
-        else  ALUControl = 3'b110; // extract out SrcB, op5 == 1
+        if (op5 == 0) ALUControlD = 3'b000; // add
+        else  ALUControlD = 3'b110; // extract out SrcB, op5 == 1
     end
-    else ALUControl = 3'b000;
+    else ALUControlD = 3'b000;
 end
 
 endmodule
