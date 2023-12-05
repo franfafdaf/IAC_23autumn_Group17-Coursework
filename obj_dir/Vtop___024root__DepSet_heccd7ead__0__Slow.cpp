943cd2bd7c502d4c486943a4c8298d2aaff27d34
lgxi24 <lambert.lgx@gmail.com>
2023-12-05 21:42:19 +0000

    test

--- obj_dir/Vtop___024root__DepSet_heccd7ead__0__Slow.cpp
+++ obj_dir/Vtop___024root__DepSet_heccd7ead__0__Slow.cpp
@@ -11,9 +11,14 @@ VL_ATTR_COLD void Vtop___024root___initial__TOP__0(Vtop___024root* vlSelf) {
     Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
     VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___initial__TOP__0\n"); );
     // Init
+    VlWide<4>/*127:0*/ __Vtemp_hfc8f3e90__0;
     VlWide<3>/*95:0*/ __Vtemp_h2553e7c1__0;
     // Body
-    VL_READMEM_N(true, 8, 4096, 0, std::string{"pdf.hex"}
+    __Vtemp_hfc8f3e90__0[0U] = 0x2e6d656dU;
+    __Vtemp_hfc8f3e90__0[1U] = 0x6d656d31U;
+    __Vtemp_hfc8f3e90__0[2U] = 0x6e737472U;
+    __Vtemp_hfc8f3e90__0[3U] = 0x69U;
+    VL_READMEM_N(true, 8, 4096, 0, VL_CVT_PACK_STR_NW(4, __Vtemp_hfc8f3e90__0)
                  ,  &(vlSelf->top__DOT__my_InstrD_mem__DOT__rom_array)
                  , 0, ~0ULL);
     __Vtemp_h2553e7c1__0[0U] = 0x2e6d656dU;
