59067ab681233cbd59a86a1a862930d3a6af97ea
lgxi24 <lambert.lgx@gmail.com>
2023-12-06 18:10:06 +0000

    updated test

--- top_tb.cpp
+++ top_tb.cpp
@@ -18,7 +18,7 @@ int main(int argc, char **argv, char **env){
   tfp->open ("top.vcd");
  
   if (vbdOpen()!=1) return(-1);
-  vbdHeader("Reference");
+  vbdHeader("SingleCycle Ref");
 
   top->clk = 1;
   top->rst = 1;
