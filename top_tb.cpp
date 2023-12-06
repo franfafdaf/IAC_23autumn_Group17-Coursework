c8f9e49751104ec671516959ace39223f795c1b6
Frank_enstein <frankfan114@outlook.com>
2023-12-06 17:15:35 +0000

    testbench modify

--- top_tb.cpp
+++ top_tb.cpp
@@ -18,11 +18,11 @@ int main(int argc, char **argv, char **env){
   tfp->open ("top.vcd");
  
   if (vbdOpen()!=1) return(-1);
-  vbdHeader("Pipelined");
+  vbdHeader("Pipelined_F1");
 
   top->clk = 1;
-  top->rst = 0;
-  int plot =0;
+  top->rst = 1;
+  top->trigger = 0;
 
   for (simcyc=0; simcyc<MAX_SIM_CYC; simcyc++) {
     // dump variables into VCD file and toggle clock
@@ -32,19 +32,13 @@ int main(int argc, char **argv, char **env){
       top->eval ();
     }
 
-    if (plot == 0 && top->a0 != 0) {
-       plot = 1;
-    }
-    // plot ROM output and print cycle count
-    if (plot >= 1) {
-       vbdPlot(int(top->a0), 0, 255);
-       vbdCycle(simcyc);
-       plot += 1;
-    }
-    if (plot > 960) {
-      break;
-    }
- 
+    top->rst = 0;
+    uint32_t value_32bit = top->a0;
+    uint8_t data_out = static_cast<uint8_t>(value_32bit & 0xFF);
+    vbdBar(data_out && 0xFF);
+
+    top->trigger = vbdFlag();
+    vbdCycle(simcyc);
 
     // either simulation finished, or 'q' is pressed
     if ((Verilated::gotFinish()) || (vbdGetkey()=='q')) 
