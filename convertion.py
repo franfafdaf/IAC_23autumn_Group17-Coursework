96df326159870f6a9852ad07edc37ff9c4ae9110
Frank_enstein <frankfan114@outlook.com>
2023-12-10 14:06:04 +0000

    add graph

--- convertion.py
+++ convertion.py
@@ -8,11 +8,17 @@ def convert_mem_to_excel(mem_file, excel_file):
     # Split the data into words separated by spaces or newlines
     words = data.replace('\n', ' ').split()
 
-    # Create a DataFrame from the list of words
-    df = pd.DataFrame(words, columns=['Data'])
+    # Convert hexadecimal values to decimal
+    decimal_values = [int(word, 16) for word in words]
+
+    # Create a DataFrame from the list of decimal values
+    df = pd.DataFrame({'Data': decimal_values})
 
     # Write the DataFrame to an Excel file
     df.to_excel(excel_file, index=False)
 
 # Example usage
-convert_mem_to_excel('sine.mem', 'sine.xlsx')
+convert_mem_to_excel('gaussian.mem', 'gaussian.xlsx')
+
+# Note: You need to have the pandas library installed to use this code.
+# Also, the 'sine.mem' file should exist in the same directory as your script, or provide the full path.
