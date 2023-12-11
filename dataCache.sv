b39c490bbcb3fd8aaaaec6ab9ec91d6fc19307ec
Frank_enstein <frankfan114@outlook.com>
2023-12-11 10:26:17 +0000

    modify

--- dataCache.sv
+++ dataCache.sv
@@ -1,17 +1,24 @@
 module dataCache #( // 2-way, 8 set, block size 4 bytes
-    arameter Tag_Width = 25, 
+    parameter Tag_Width = 25, 
     parameter Set_Width = 3,
     parameter BlockOffset = 2,
     parameter ByteOffset = 2,
     parameter Data_Width =32
 ) (
     input logic                             clk,
-    input logic [Data_Width-1:0]            data_in,    
+    input logic [Data_Width-1:0]            dataIn,    
     input logic [Data_Width-1:0]            addressIn,  
     input logic                             we,                  
 
-    output logic [Data_Width-1:0]           data_out,   
+    output logic [Data_Width-1:0]           dataOut,   
     output logic                            hit_out,
+
+    input  logic                            WE,          // Write Enable signal
+    input  logic                            StSrcM,       // Store Type signal
+    input  logic                            LdSrcM,       // Load Type signal
+    input  logic [DATA_WIDTH-1:0]           WD,          // Write Data input
+    
+    output logic [DATA_WIDTH-1:0]           RD           // Read Data output
 );
     logic                   valid;
     logic                   hit;
@@ -30,6 +37,15 @@ module dataCache #( // 2-way, 8 set, block size 4 bytes
     logic [(2+1+2*Tag_Width+2*4*Data_Width):0] cacheMem [7:0];
     logic [(2+1+2*Tag_Width+2*4*Data_Width):0] selectedWay;
 
+    DataMemory my_data_memory(
+    .clk(clk),
+    .WE(WE),
+    .StSrcM(StSrcM),
+    .LdSrcM(LdSrcM),
+    .A(ALURaesultM),
+    .WD(WriteDataM),
+    .RD(RD)
+    );
     // input 
     assign inputTag = addressIn[Data_Width-1:Data_Width-Tag_Width];
     assign inputSet = addressIn[(Data_Width-Tag_Width-1): (Data_Width-Tag_Width-1-Set_Width)];
@@ -73,7 +89,40 @@ module dataCache #( // 2-way, 8 set, block size 4 bytes
 
 
 
-    // if hit
+
+
+   
+    // read/load data
+    always_comb begin
+        if(hit)begin  // if hit
+            if(hit1) begin // way1 hit
+                if (LdSrcM) dataOut = {{24{1'b0}}, way1out[7:0]}; //LBU
+                else dataOut = {way1out}; //LW
+            end
+
+            else begin // way0 hit
+                if (LdSrcM) dataOut = {{24{1'b0}}, way0out[7:0]}; //LBU
+                else dataOut = {way0out}; //LW
+            end
+        end
+        else begin
+            if(used = 0)begin
+                if(LdSrcM) selectBlock = {Rd} 
+                used = 1; 
+            end
+            else begin
+
+                used = 0; 
+            end
+
+        end
+    end
+ 
+
+    //if miss
+    //write
+    //read
+
 
 
     //output select
@@ -95,10 +144,10 @@ module dataCache #( // 2-way, 8 set, block size 4 bytes
 
         if(hit)begin
             if(hit1)begin
-                data_out = way1out;
+                dataOut = way1out;
             end
             else begin
-                data_out =way0out;
+                dataOut =way0out;
             end
         end
         else begin
@@ -106,11 +155,4 @@ module dataCache #( // 2-way, 8 set, block size 4 bytes
         end
     end
 
-    // write
-    always_comb begin
-        if(hit != 1)begin
-             
-        end
-    end
-    
 endmodule
