99410d5778004d4eaa077cf5fba19e700e81bbe9
Frank_enstein <frankfan114@outlook.com>
2023-12-13 09:57:43 +0000

    modify

---
+++ top_tb.cpp
@@ -0,0 +1,64 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vtop.h"
+#include "vbuddy.cpp"    
+#define MAX_SIM_CYC 1000000
+
+int main(int argc, char **argv, char **env){
+  int simcyc;
+  int tick;
+  int count =0; 
+
+  Verilated::commandArgs(argc, argv);
+
+  Vtop * top = new Vtop;
+
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  top->trace (tfp, 99);
+  tfp->open ("top.vcd");
+ 
+  if (vbdOpen()!=1) return(-1);
+  vbdHeader("CacheRef");
+
+  top->clk = 1;
+  top->rst = 0;
+  int plot =0;
+
+  for (simcyc=0; simcyc<MAX_SIM_CYC; simcyc++) {
+    // dump variables into VCD file and toggle clock
+    for (tick=0; tick<2; tick++) {
+      tfp->dump (2*simcyc+tick);
+      top->clk = !top->clk;
+      top->eval ();
+    }
+
+    if (plot == 0 && top->a0 != 0) {
+       plot = 1;
+    }
+    // plot ROM output and print cycle count
+    if (plot >= 1) {
+      if(count == 3){
+        vbdPlot(int(top->a0), 0, 255);
+        count =0;
+        vbdCycle(simcyc);
+      }
+      else{
+        count = count +1;
+      }
+      plot += 1;
+    }
+    if (plot > 960) {
+      break;
+    }
+ 
+
+    // either simulation finished, or 'q' is pressed
+    if ((Verilated::gotFinish()) || (vbdGetkey()=='q')) 
+      exit(0);
+  }
+
+  vbdClose();     
+  tfp->close(); 
+  exit(0);
+}
